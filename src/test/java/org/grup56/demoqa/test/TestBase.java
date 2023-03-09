cc2ca8b7095363ff00de8649e91cc799dccca1f8
onurbapur <onurrbapurr@gmail.com>
2023-03-09 19:51:27 +0300

    Proje 09/03/2023 tarihinde oluşturuldu.

---
+++ src/test/java/org/grup56/demoqa/test/TestBase.java
@@ -0,0 +1,79 @@
+package org.grup56.demoqa.test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.grup56.demoqa.utilities.BrowserUtils;
+import org.grup56.demoqa.utilities.ConfigurationReader;
+import org.grup56.demoqa.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+   protected WebDriver driver;
+   protected WebDriverWait wait;
+   protected Actions actions;
+
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+        report = new ExtentReports();
+
+        String projectPath= System.getProperty("user.dir");
+        String path= projectPath + "/test-output/report.html";
+
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        report.attachReporter(htmlReporter);
+
+        htmlReporter.config().setReportName("Audit Smoke Test");
+
+        //Set environment information --> Test Name, Tester Name, Browser, Test Steps (Admin/User), Test Data, Date and Time, Operating System, Result
+        report.setSystemInfo("Environment","Production");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS", System.getProperty("os.name"));
+        report.setSystemInfo("Test Engineer","Ihsan");
+
+    }
+
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = Driver.get();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(Driver.get(),10);
+        actions = new Actions(driver);
+        driver.get(ConfigurationReader.get("url"));
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws InterruptedException, IOException {
+
+        if (result.getStatus()==ITestResult.FAILURE){
+            extentLogger.fail(result.getName());
+
+            String screenshotPath= BrowserUtils.getScreenshot(result.getName());
+            extentLogger.addScreenCaptureFromPath(screenshotPath);
+            extentLogger.fail(result.getThrowable());
+
+        }
+
+        Driver.closeDriver();
+    }
+}
