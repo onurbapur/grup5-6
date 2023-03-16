68b544268c312d27f7ce0a766d64f82116a93cc3
onurbapur <onurrbapurr@gmail.com>
2023-03-16 22:26:33 +0300

    commit 16.03 22.26

---
+++ src/test/java/org/grup56/demoqa/test/task03_CheckBox/CheckBoxPositiveTest.java
@@ -0,0 +1,22 @@
+package org.grup56.demoqa.test.task03_CheckBox;
+
+public class CheckBoxPositiveTest {
+     /*  Test1
+    -Navigate to https://demoqa.com/
+    -Click to Elements
+    -Click to Check Box
+    -Click on home file
+    -Verify this note"You have selected :home desktop notes commands documents workspace react angular veu
+    office public private classified general downloads wordFile excelFile "
+    */
+
+
+    /* Test2
+    -Navigate to https://demoqa.com/
+    -Click to Elements
+    -Click to Check Box
+    -Click to (+)Plus button and click to Download toggle and verify Word File.doc Excel File.doc
+    -Click to Excel File.doc box verify that "You have selected : excelFile"
+
+    * */
+}
